a75b9cb555a7e4dd4bcd71632b92d9ff09bd3d1d
Yvette <52262091+yvetteingram@users.noreply.github.com>
2020-09-30 15:18:10 -0400

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div style={{ textAlign: "center", color: "white" }}>
+      <h3>Made by Yvette Ketorah Ingram</h3>
+      <a href="mailto:ketorah.dev@gmail.com">
+        Contact Developer Yvette Ketorah Ingram
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
